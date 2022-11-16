bf3db0584de31c213a02bc2a920b5251d78e10d1
Fayi Muhadzdzib <fayimujahid1@gmail.com>
2022-11-16 10:38:51 +0700

    tutor finish

---
+++ variabel super global/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+    // Variabel $_SERVER
+    // Berguna untuk menampilkan informasi tentang server yang di gunakan 
+    // echo json_encode($_SERVER);
+
+    // Variabel $_POST
+    // Passing data dari body request 
+
+    // Varabel $_SESSION
+    // Variabel yang menyimpan data pengguna, jadi jika pengguna mengunjungi situs tersebut dengan waktu tertentu, data akan kembali tersaji tanpa harus di isi kembali
+    // Session bisa di atur waktunya, secara defaultnya 24 menit
+
+    // Variabel $_COOKIE
+    // durasinya lebih panjang dari session
+
+    // Variabel $_REQUEST
+    // menyimpan gabungan nilai dari $_GET, $_POST, $_COOKIE yang di kirim bersamaan dengan request user
+
+    // Variabel $_FILES
+    // Biasanya digunakan untuk fitur upload file
+
+    // Variabel $_ENV
+    // digunakan dalam framework yang berisi database.env
+
+    // Variabel $_GLOBALS
+    // biar bisa di akses ke semua file
+?>
