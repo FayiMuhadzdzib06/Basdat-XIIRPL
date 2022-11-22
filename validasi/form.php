b01c4d9137d78d95b8c18ddab2ba429166e80bd5
Fayi Muhadzdzib <fayimujahid1@gmail.com>
2022-11-22 14:22:21 +0700

    validasi dah kelar

--- validasi/form.php
+++ validasi/form.php
@@ -1,3 +1,8 @@
+<?php 
+    $time = (object) @$_GET['time'];
+    $errors = (object) @$_GET['errors'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,20 +15,48 @@
     <form action="proses.php">
         <div>
             <label>Nama( Harus Input nama )</label><br>
-            <input type="text" name="nama">
+            <input type="text" value="<?php echo @$time->nama?>" name="nama"><br>
+            <?php 
+                if(@$errors->nama):?>
+                <div style="color: red;"><?php echo $errors->nama[0] ?></div>
+                <?php
+                endif;
+            ?>
+            <br>
         </div>
         <div style="margin: top 10px;">
             <label>Email( Harus Input @ )</label><br>
-            <input type="text" name="email">
+            <input type="text" value="<?php echo @$time->email?>" name="email"><br>
+            <?php 
+                if(@$errors->email):?>
+                <div style="color: red;"><?php echo $errors->email[0] ?></div>
+                <?php
+                endif;
+            ?>
+            <br>
         </div>
         <div style="margin: top 10px;">
             <label>Username( Tidak Boleh Regex )</label><br>
-            <input type="text" name="username">
+            <input type="text" value="<?php echo @$time->username?>" name="username"><br>
+            <?php 
+                if(@$errors->username):?>
+                <div style="color: red;"><?php echo $errors->username[0] ?></div>
+                <?php
+                endif;
+            ?>
+            <br>
         </div>
         <div style="margin: top 10px;">
             <label>Usia( Harus Angka )</label><br>
-            <input type="text" name="usia">
-        </div><br>
+            <input type="text" value="<?php echo @$time->usia?>" name="usia"><br>
+            <?php 
+                if(@$errors->usia):?>
+                <div style="color: red;"><?php echo $errors->usia[0] ?></div>
+                <?php
+                endif;
+            ?>
+            <br>
+        </div>
         <div style="margin: top 10px;">
         <button>Submit</button>
         </div>
